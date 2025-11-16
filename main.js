9c4e36eaf9fb1fab732c78f3d442b43a37127eb1
camiilo laptop <camieros@live.com>
2025-11-16 13:04:53 -0500

    Removed the buttons, and made the logic on the error catch when other number is the same

--- main.js
+++ main.js
@@ -1,27 +1,38 @@
 //Variables de las casillas de juego
 let input1 = document.querySelector("#input1");
+let input2 = document.querySelector("#input2");
+let input3 = document.querySelector("#input3");
 
 
 
-//Variables de los botones de numeros
-let btn1 = document.querySelector("#btn-one");
 
-
-
+const maxLength = 1; // Set your desired character limit
 
 
 //Listeners
-input1.addEventListener("input", print);
-btn1.addEventListener("clic", insertone);
+input1.addEventListener("input", print,);
+input1.addEventListener("input", funcvalidate);
+input3.addEventListener("input", print);
 
 
 
 
 //Funciones
-function print() {
-    console.log(input1.value);
-}
+function print() {//funcion que limita a 1 la cantidad de digitos que puede ingresar el jugador
+    if (input1.value.length >= maxLength) {
+        input1.value = input1.value.slice(0, maxLength);
+        }
+        console.log(input1.value);
+    };
 
-function insertone() {
-    
+function funcvalidate(params) {
+    if (input2.value === input1.value) {
+        input1.style.backgroundColor = "red";
+    }
 }
+// funcion para confirmar que el input en el que se vaya a agregar el numero este seleccionado:
+//if (input1 === document.activeElement) {
+//        console.log('The input field is focused!');
+//    } else {
+//        console.log('The input field is not focused.');
+//    }
